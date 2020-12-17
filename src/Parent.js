38545250bd032ad11e6e4227af2e834cf9494568
Gabbi Nguyen <gabbinguyen@gmail.com>
2020-12-17 12:05:59 -0600

    commit tree b03951c26a6dd585f4b3a30827622504ff92b0e4

--- src/Parent.js
+++ src/Parent.js
@@ -11,11 +11,17 @@ class Parent extends Component {
     }
   }
 
+  changeColor = () => {
+    this.setState({
+      color: getRandomColor()
+    })
+  }
+
   render() {
     return (
       <div className="parent" style={{backgroundColor: this.state.color}}>
-        <Child />
-        <Child />
+        <Child handleChangeColor={this.changeColor} />
+        <Child handleChangeColor={this.changeColor} />
       </div>
     )
   }
